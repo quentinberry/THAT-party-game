b68e6c35d6d427c1b0da85a334d521705c481b1e
quentinberry <34132543+quentinberry@users.noreply.github.com>
2022-08-02 20:30:45 +0200

    WIP

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { LogBox, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Button } from "react-native-web";
 
 export default function App() {
-  const onPress = () => console.log("Casual mode has been clicked");
+  const onPress = () => console.log("Some mode has been clicked");
+  const changeColor = () => console.log("Change Color pls");
 
   return (
     <View style={styles.background}>
@@ -14,9 +15,28 @@ export default function App() {
       </View>
 
       <View style={styles.gameMenu}>
-        <TouchableOpacity style={styles.casualModeButton} onPress={onPress}>
-          <Text style={styles.causalModeText}>Casual Mode</Text>
-        </TouchableOpacity>
+        <View style={styles.casualModeBackground}>
+
+          <TouchableOpacity style={styles.casualModeInfo} onPress={onPress}>
+            <Text style={styles.causalModeText}>Casual Mode</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.startButton} onPress={changeColor}>
+            <Text style={styles.startButtonText}>Select</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.fastModeBackground}>
+
+          <TouchableOpacity style={styles.fastModeInfo} onPress={onPress}>
+            <Text style={styles.fastModeText}>Fast Mode</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.startButton} onPress={changeColor}>
+            <Text style={styles.startButtonText}>Select</Text>
+          </TouchableOpacity>
+        </View>
+
       </View>
 
     </View>
@@ -46,8 +66,17 @@ const styles = StyleSheet.create({
     marginLeft: 16,
     marginRight: 16,
     marginTop:45,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
-  casualModeButton:{
+  casualModeBackground:{
+    backgroundColor: "#F4F7FB",
+    borderRadius: 22,
+    width:171,
+    height:237,
+  },
+  casualModeInfo:{
     width:171,
     height:171,
     borderRadius:22,
@@ -61,5 +90,43 @@ const styles = StyleSheet.create({
     paddingLeft: 13,
     paddingRight:13,
     paddingBottom: 24,
-  },  
+  },
+  fastModeBackground:{
+    backgroundColor: "#F4F7FB",
+    borderRadius: 22,
+    width:171,
+    height:237,
+    marginRight: 16,
+  },
+  fastModeInfo:{
+    width:171,
+    height:171,
+    borderRadius:22,
+    backgroundColor:"#8BF2E6"
+  },
+  fastModeText:{
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontSize: 24,
+    paddingTop: 96,
+    paddingLeft: 13,
+    paddingRight:13,
+    paddingBottom: 24,
+  },
+  startButton:{
+    backgroundColor: "#CFCFCF",
+    width: 145,
+    height:33,
+    marginLeft:13,
+    marginTop:15,
+    borderRadius:30,
+  },
+  startButtonText:{
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontSize: 24,
+    textAlign: 'center',
+    color: "#fff",
+    marginTop:3,
+  }
 });
