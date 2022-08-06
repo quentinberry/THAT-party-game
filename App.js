5fa9bdc107f6e951e012fe47d2b5eaf8510f546d
quentinberry <34132543+quentinberry@users.noreply.github.com>
2022-08-06 18:29:04 +0200

    done poc

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   const onPress = () => console.log("Casual mode has been clicked");
+  const onButtonPress = () => console.log("Select button ahs been pressed");
 
   return (
     <View style={styles.background}>
@@ -12,10 +13,60 @@ export default function App() {
         <Text style={styles.realTitle}>THAT Party Game</Text>
       </View>
 
-      <View style={styles.gameMenu}>
-        <TouchableOpacity style={styles.casualModeButton} onPress={onPress}>
-          <Text style={styles.causalModeText}>Casual Mode</Text>
-        </TouchableOpacity>
+      <View style={styles.gameMenuTop}>
+        <View style={styles.gameMenuBackground}>
+
+          <TouchableOpacity style={styles.casualModeBackground} onPress={onPress}>
+           <Text style={styles.modeText}>Casual</Text>
+           <Text style={styles.modeTextPart}>Mode</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.selectButton} onPress={onButtonPress}>
+            <Text style={styles.selection}>Select</Text>
+          </TouchableOpacity>
+
+        </View>
+
+        <View style={styles.gameMenuBackground}>
+
+          <TouchableOpacity style={styles.fastModeBackground} onPress={onPress}>
+           <Text style={styles.modeText}>Fast</Text>
+           <Text style={styles.modeTextPart}>Mode</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.selectButton} onPress={onButtonPress}>
+            <Text style={styles.selection}>Select</Text>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+
+      <View style={styles.gameMenuBottom}>
+        <View style={styles.gameMenuBackground}>
+
+          <TouchableOpacity style={styles.spicyModeBackground} onPress={onPress}>
+           <Text style={styles.modeText}>Spicy</Text>
+           <Text style={styles.modeTextPart}>Mode</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.selectButton} onPress={onButtonPress}>
+            <Text style={styles.selection}>Select</Text>
+          </TouchableOpacity>
+
+        </View>
+
+        <View style={styles.gameMenuBackground}>
+
+          <TouchableOpacity style={styles.partnerModeBackground} onPress={onPress}>
+           <Text style={styles.modeText}>Partner</Text>
+           <Text style={styles.modeTextPart}>Mode</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.selectButton} onPress={onButtonPress}>
+            <Text style={styles.selection}>Select</Text>
+          </TouchableOpacity>
+
+        </View>
       </View>
 
     </View>
@@ -41,24 +92,88 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontStyle: "normal",
   },
-  gameMenu: {
+  gameMenuTop: {
     marginLeft: 16,
     marginRight: 16,
     marginTop:45,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  casualModeButton:{
+  gameMenuBackground: {
+    backgroundColor: "#f4f7fb",
+    borderRadius: 22,
+    width:171,
+    height:237,
+  },
+  casualModeBackground:{
     width:171,
     height:171,
     borderRadius:22,
-    backgroundColor:"#8BB7F2"
+    backgroundColor:"#8BB7F2",
   },
-  causalModeText:{
+  modeText:{
     fontFamily: "Montserrat",
     fontStyle: "normal",
     fontSize: 24,
     paddingTop: 96,
     paddingLeft: 13,
     paddingRight:13,
-    paddingBottom: 24,
-  },  
+    fontWeight:700,
+  },
+  modeTextPart:{
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontSize:24,
+    fontWeight:400,
+    paddingRight:13,
+    paddingLeft: 13,
+  },
+  selectButton:{
+    width: 145,
+    height: 33,
+    backgroundColor: "#cfcfcf",
+    borderRadius: 22,
+    marginLeft:13,
+    marginTop: 15,
+  },
+  selection:{
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontSize:24,
+    width:91,
+    fontWeight:400,
+    color: '#fff',
+    alignItems: "center",
+    alignSelf: "center",
+    display: "flex",
+    marginLeft:27,
+    height: 33,
+  },
+  fastModeBackground:{
+    width:171,
+    height:171,
+    borderRadius:22,
+    backgroundColor:"#8BF2E6",
+  },
+  gameMenuBottom:{
+    marginLeft: 16,
+    marginRight: 16,
+    marginTop:16,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  spicyModeBackground:{
+    width:171,
+    height:171,
+    borderRadius:22,
+    backgroundColor:"#E4F28B",
+  },
+  partnerModeBackground:{
+    width:171,
+    height:171,
+    borderRadius:22,
+    backgroundColor:"#F28B8B",
+  },
 });
