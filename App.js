cb9b170b95e8321485ed652e28abb954961ea25a
quentinberry <34132543+quentinberry@users.noreply.github.com>
2022-08-07 20:12:30 +0200

    make more responsive and create component for game mode

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { useFonts, Montserrat_400Regular, Montserrat_600SemiBold} from "@expo-google-fonts/montserrat";
-import AppLoading from "expo-app-loading";
+import GameMode from "./component/GameMode";
 
 export default function App() {
-
-  let [fontsLoaded] = useFonts({
-    Montserrat_400Regular,
-    Montserrat_600SemiBold,
-  });
-
-  if (!fontsLoaded){
-    return <AppLoading />;
-  }
-
-  const onPress = () => console.log("Casual mode has been clicked");
-  const onButtonPress = () => console.log("Select button ahs been pressed");
-
   return (
     <View style={styles.background}>
       <StatusBar style="auto" />
@@ -26,65 +12,16 @@ export default function App() {
       </View>
 
       <View style={styles.allGamesWrapper}>
-
-
-      <View style={styles.gameMenuTop}>
-        <View style={styles.gameMenuBackground}>
-
-          <TouchableOpacity style={styles.casualModeBackground} onPress={onPress}>
-           <Text style={styles.modeText}>Casual</Text>
-           <Text style={styles.modeTextPart}>Mode</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.selectButton} onPress={onButtonPress}>
-            <Text style={styles.selection}>Select</Text>
-          </TouchableOpacity>
-
+        <View style={styles.gameMenuTop}>
+          <GameMode text={"Casual"} />
+          <GameMode text={"Fast"} />
         </View>
 
-        <View style={styles.gameMenuBackground}>
-
-          <TouchableOpacity style={styles.fastModeBackground} onPress={onPress}>
-           <Text style={styles.modeText}>Fast</Text>
-           <Text style={styles.modeTextPart}>Mode</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.selectButton} onPress={onButtonPress}>
-            <Text style={styles.selection}>Select</Text>
-          </TouchableOpacity>
-
+        <View style={styles.gameMenuBottom}>
+          <GameMode text={"Spicy"} />
+          <GameMode text={"Partner"} />
         </View>
       </View>
-
-      <View style={styles.gameMenuBottom}>
-        <View style={styles.gameMenuBackground}>
-
-          <TouchableOpacity style={styles.spicyModeBackground} onPress={onPress}>
-           <Text style={styles.modeText}>Spicy</Text>
-           <Text style={styles.modeTextPart}>Mode</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.selectButton} onPress={onButtonPress}>
-            <Text style={styles.selection}>Select</Text>
-          </TouchableOpacity>
-
-        </View>
-
-        <View style={styles.gameMenuBackground}>
-
-          <TouchableOpacity style={styles.partnerModeBackground} onPress={onPress}>
-           <Text style={styles.modeText}>Partner</Text>
-           <Text style={styles.modeTextPart}>Mode</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.selectButton} onPress={onButtonPress}>
-            <Text style={styles.selection}>Select</Text>
-          </TouchableOpacity>
-
-        </View>
-      </View>
-      </View>
-
     </View>
   );
 }
@@ -95,98 +32,33 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     paddingTop: 94,
-    paddingLeft: 16,
-    paddingRight: 16,
+    paddingLeft: '4%',
+    paddingRight: '4%',
   },
   topperTitle: {
-    fontFamily: "Montserrat_400Regular",
+    fontFamily: "Montserrat",
     fontSize: 24,
     fontStyle: "normal",
   },
   realTitle: {
-    fontFamily: "Montserrat_600SemiBold",
+    fontFamily: "Montserrat",
     fontSize: 32,
     fontStyle: "normal",
   },
-  allGamesWrapper:{
-    marginLeft: 16,
-    marginRight: 16,
-    marginTop:45,
+  allGamesWrapper: {
+    marginLeft: '4%',
+    marginRight:'4%',
+    marginTop: 45,
   },
   gameMenuTop: {
-    
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  gameMenuBackground: {
-    backgroundColor: "#f4f7fb",
-    borderRadius: 22,
-    width:171,
-    height:237,
-  },
-  casualModeBackground:{
-    width:171,
-    height:171,
-    borderRadius:22,
-    backgroundColor:"#8BB7F2",
-  },
-  modeText:{
-    fontFamily: "Montserrat_400Regular",
-    fontStyle: "normal",
-    fontSize: 24,
-    paddingTop: 96,
-    paddingLeft: 13,
-    paddingRight:13,
-  },
-  modeTextPart:{
-    fontFamily: "Montserrat_600SemiBold",
-    fontStyle: "normal",
-    fontSize:24,
-    paddingRight:13,
-    paddingLeft: 13,
-  },
-  selectButton:{
-    width: 145,
-    height: 33,
-    backgroundColor: "#cfcfcf",
-    borderRadius: 22,
-    marginLeft:13,
-    marginTop: 15,
-  },
-  selection:{
-    fontFamily: "Montserrat",
-    fontStyle: "normal",
-    fontSize:24,
-    width:91,
-    color: '#fff',
-    alignItems: "center",
-    alignSelf: "center",
-    display: "flex",
-    marginLeft:27,
-    height: 33,
-  },
-  fastModeBackground:{
-    width:171,
-    height:171,
-    borderRadius:22,
-    backgroundColor:"#8BF2E6",
-  },
-  gameMenuBottom:{
+  gameMenuBottom: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-  },
-  spicyModeBackground:{
-    width:171,
-    height:171,
-    borderRadius:22,
-    backgroundColor:"#E4F28B",
-  },
-  partnerModeBackground:{
-    width:171,
-    height:171,
-    borderRadius:22,
-    backgroundColor:"#F28B8B",
+    marginTop: 16+237,
   },
 });
---
+++ GlobalStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    casualModeColor:{
+        color: '#8BB7F2',
+    },
+    fastModeColor:{
+        color: '#8BF2E6',
+    },
+    spicyModeColor:{
+        color: '#E4F28B',
+    },
+    parnterModeColor:{
+        color: '#F28B8B',
+    },
+});
